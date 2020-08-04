582d714d841bcefa650e29bfe3104d3a65a6a8bd
ellen <ellen6666@naver.com>
2020-08-04 15:18:21 +0900

    MAP - succeed to make markers on map

--- frontend/src/views/post/Recommend.vue
+++ frontend/src/views/post/Recommend.vue
@@ -28,11 +28,42 @@ export default {
     },
     methods:{
         initTmap(){
-           this.map = new Tmap.Map({
-               div:'map_div', 
+           this.map = new Tmapv2.Map('map_div',{
+               center : new Tmapv2.LatLng(37.570028, 126.986072),
                width:'100%', 
-               height:'630px'});
-        this.map.setCenter(new Tmap.LonLat(126.983045, 37.572194).transform("EPSG:4326", "EPSG:3857"), 15);
+               height:'400px',
+               zoom : 15,
+			zoomControl : true,
+			scrollwheel : true});
+        
+        this.marker_s = new Tmapv2.Marker(
+				{
+					position : new Tmapv2.LatLng(37.569758, 126.977022),
+					icon : "http://v2apis.sktelecom.com/upload/tmap/marker/pin_r_m_s.png",
+					iconSize : new Tmapv2.Size(24, 38),
+					map : this.map
+                });
+        this.marker_e = new Tmapv2.Marker(
+				{
+					position : new Tmapv2.LatLng(37.570594, 126.997589),
+					icon : "http://tmapapis.sktelecom.com/upload/tmap/marker/pin_r_m_e.png",
+					iconSize : new Tmapv2.Size(24, 38),
+					map : this.map
+                });
+        this.marker_p1 = new Tmapv2.Marker(
+				{
+					position : new Tmapv2.LatLng(37.573028, 126.983072),
+					icon : "http://tmapapis.sktelecom.com/upload/tmap/marker/pin_b_m_p.png",
+					iconSize : new Tmapv2.Size(24, 38),
+					map : this.map
+                });
+        this.marker_p2 = new Tmapv2.Marker(
+				{
+					position : new Tmapv2.LatLng(37.565778, 126.987319),
+					icon : "http://tmapapis.sktelecom.com/upload/tmap/marker/pin_b_m_p.png",
+					iconSize : new Tmapv2.Size(24, 38),
+					map : this.map
+				});
 
 
     }
@@ -40,12 +71,12 @@ export default {
 
     ,
     mounted(){
-        if (window.Tmap && window.Tmap.Map) {
+        if (window.Tmapv2 && window.Tmapv2.Map) {
         this.initTmap();
       } else {
         const script = document.createElement('script');
-        /* global Tmap */
-        script.onload = () => Tmap.Map.load(this.initTmap);
+        /* global Tmapv2 */
+        script.onload = () => Tmapv2.Map.load(this.initTmap);
         script.src = "https://apis.openapi.sk.com/tmap/js?version=1&format=javascript&appKey=l7xx85f17a9a757349b59a2d9eb9d1382cb3";
         document.head.appendChild(script);
       }
