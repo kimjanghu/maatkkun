0d7a0811326d96e108bf394072c801061809283b
이원호 <dldnjsgh71@naver.com>
2020-08-05 10:14:51 +0900

    RECOMMEND - crawler.java

--- backend/src/main/java/com/web/blog/util/Crawler.java
+++ backend/src/main/java/com/web/blog/util/Crawler.java
@@ -1,44 +1,68 @@
 package com.web.blog.util;
 
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Crawler {
 
-/*
-public static void main(String[] args) {
-    
-    final String menu = "할리스";
-    final String url = "https://place.map.kakao.com/1969587022";
-    try{
-        Document doc = Jsoup.connect(url).get();
-        System.out.println(doc.html());
-    }catch(Exception e){
-        System.out.println(e.getMessage());
-    }
-}
-*/
-// 1596523926447
-// 1596524019896
-    
+    public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+    public static final String WEB_DRIVER_PATH = "C:\\Users\\multicampus\\Desktop\\sel\\chromedriver.exe";
+        
     public static void main(final String[] args) {
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://place.map.kakao.com/1989327162";
+        driver.get(url);
+        
+
+
+        
+        driver.get(url);
+        List<WebElement> elements = null;
+        while (elements == null)
+        {
+            elements = driver.findElements(By.className("marker"));
+            if (!valuePresent(elements))
+            {
+                elements = null;
+            }
+        }
+
+        if (elements != null)
+        {
+            processElements(elements);
+        }
+
+
+
+
         // final String menu = "할리스";
         // final String url = "https://www.google.com/maps/search/"+menu;
 
-
-        final String menu = "할리스역삼점";
-        // final String url = "https://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q="+menu;
-        final String url = "https://place.map.kakao.com/1989327162#kakaoBody";
-        
         // System.out.println(url);
+
+
+
+
 
         
         try{
-            Document doc = Jsoup.connect(url).get();
+            // for(int i = 0; i< 2; i++){
+            //     Document doc = Jsoup.parse(driver.getPageSource());
+            //     System.out.println(doc.body().html());  
+            //     Thread.sleep(1000);
+            //     System.out.println("================================================================================");
+            // }
             
-            System.out.println(doc.body().html());
             // Element el = doc.getElementById("kakaoIndex");
             // System.out.println(el);
             // Elements el = doc.getElementsByClass("fn_tit");
@@ -54,7 +78,7 @@ public static void main(String[] args) {
 
             //         //link_evaluation
             //         break;
-            //     }
+            //     }~
             //     System.out.println("=================================================");
             // }
             
