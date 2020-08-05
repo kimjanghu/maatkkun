9787cc5af7c888541aaacc2263b32349c74858d8
이원호 <dldnjsgh71@naver.com>
2020-08-05 12:36:00 +0900

    RECOMMEND - add crawler

--- backend/src/main/java/com/web/blog/controller/account/PostController.java
+++ backend/src/main/java/com/web/blog/controller/account/PostController.java
@@ -60,6 +60,9 @@ import org.jsoup.Connection.KeyVal;
 import org.jsoup.nodes.Document; 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 
@@ -105,6 +108,8 @@ public class PostController {
     @ApiOperation(value = "글 리스트", notes = "글 리스트 API")
     @GetMapping(value="/articles/list")
     public ResponseEntity<Object> getList(){
+
+        crawling();
 
         HashMap<String,Object> hm = new HashMap<>();
         
@@ -705,6 +710,41 @@ public class PostController {
         @Override
         public int compareTo(KeyValue o) {
             return (int)(this.score - o.score);
+        }
+    }
+
+    public void crawling(){
+        final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+        final String WEB_DRIVER_PATH = "C:\\Users\\multicampus\\Desktop\\sel\\chromedriver.exe";
+        // final String WEB_DRIVER_PATH = "/usr/local/bin/chromedriver";
+
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        String url = "https://place.map.kakao.com/1989327162";
+
+        
+        try{
+            driver.get(url);
+            Thread.sleep(500);
+            Document doc = Jsoup.parse(driver.getPageSource());
+            Elements el = doc.body().getElementsByClass("link_evaluation");
+            double star = Double.parseDouble(el.text().split(" ")[1].substring(0,3));
+            System.out.println(star);
+            // return star;
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally{
+            driver.close();
         }
     }
 }
--- backend/src/main/java/com/web/blog/util/Crawler.java
+++ backend/src/main/java/com/web/blog/util/Crawler.java
@@ -1,90 +1,45 @@
 package com.web.blog.util;
 
-import java.util.List;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Crawler {
-
-    public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
-    public static final String WEB_DRIVER_PATH = "C:\\Users\\multicampus\\Desktop\\sel\\chromedriver.exe";
-        
     public static void main(final String[] args) {
+        final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+        final String WEB_DRIVER_PATH = "C:\\Users\\multicampus\\Desktop\\sel\\chromedriver.exe";
+        // final String WEB_DRIVER_PATH = "/usr/local/bin/chromedriver";
+
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
+
+        WebDriver driver = new ChromeDriver(options);
 
         String url = "https://place.map.kakao.com/1989327162";
-        driver.get(url);
-        
-
-
-        
-        driver.get(url);
-        List<WebElement> elements = null;
-        while (elements == null)
-        {
-            elements = driver.findElements(By.className("marker"));
-            if (!valuePresent(elements))
-            {
-                elements = null;
-            }
-        }
-
-        if (elements != null)
-        {
-            processElements(elements);
-        }
-
-
-
-
-        // final String menu = "할리스";
-        // final String url = "https://www.google.com/maps/search/"+menu;
-
-        // System.out.println(url);
-
-
-
-
 
         
         try{
-            // for(int i = 0; i< 2; i++){
-            //     Document doc = Jsoup.parse(driver.getPageSource());
-            //     System.out.println(doc.body().html());  
-            //     Thread.sleep(1000);
-            //     System.out.println("================================================================================");
-            // }
-            
-            // Element el = doc.getElementById("kakaoIndex");
-            // System.out.println(el);
-            // Elements el = doc.getElementsByClass("fn_tit");
-            // for(Element e : el ){
-            //     System.out.println("=================================================");
-            //     if(e.text().contains("역삼")){
-
-
-            //         Document doo = Jsoup.connect(e.parent().getElementsByTag("a").attr("href")).get();
-            //         System.out.println(e.parent().getElementsByTag("a").attr("href"));
-            //         System.out.println(doo.html());
-                    
-
-            //         //link_evaluation
-            //         break;
-            //     }~
-            //     System.out.println("=================================================");
-            // }
-            
+            driver.get(url);
+            Thread.sleep(500);
+            Document doc = Jsoup.parse(driver.getPageSource());
+            Elements el = doc.body().getElementsByClass("link_evaluation");
+            double star = Double.parseDouble(el.text().split(" ")[1].substring(0,3));
+            System.out.println(star);
         }
         catch(Exception e){
             System.out.println(e.getMessage());
+        }
+        finally{
+            driver.close();
         }
         
 
