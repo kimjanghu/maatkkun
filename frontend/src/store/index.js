8342f7267a8229e3d4d81af58421e21128cb3a57
kimjanghu <hopewill1030@naver.com>
2020-08-11 18:17:39 +0900

    WEBSOCKET - fix code in store index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -162,16 +162,16 @@ export default new Vuex.Store({
     connectWebsocket({ commit }) {
       let socket = new SockJS(process.env.VUE_APP_API_URL);
       this.stompClient = Stomp.over(socket);
-      console.log(`소켓 연결을 시도합니다. 서버 주소: ${process.env.VUE_APP_API_URL}`)
-      this.stompClient.connect({}, frame => {
+      // console.log(`소켓 연결을 시도합니다. 서버 주소: ${process.env.VUE_APP_API_URL}`)
+      this.stompClient.connect({}, () => {
         // 소켓 연결 성공
         this.connected = true;
-        console.log('소켓 연결 성공', frame);
+        // console.log('소켓 연결 성공', frame);
         // 서버의 메시지 전송 endpoint를 구독합니다.
         // 이런형태를 pub sub 구조라고 합니다.
         this.stompClient.subscribe("/send", res => {
-          console.log(res);
-          console.log('구독으로 받은 메시지 입니다.', res.body);
+          // console.log(res);
+          // console.log('구독으로 받은 메시지 입니다.', res.body);
           // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
           commit('SET_RECV_DATA', JSON.parse(res.body))
           // this.recvList.push(JSON.parse(res.body))
