193908128ccad34efde37e970f754138e4534f45
kimjanghu <hopewill1030@naver.com>
2020-08-12 17:15:54 +0900

    FRONT_REFACTORING - add post sort navigation bar

---
+++ frontend/src/components/common/PostNavbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="post-list-link">
+      <div class="main-link" :class="{ active: listStatus.isRecentList }">
+        <router-link :to="{ name: constants.URL_TYPE.POST.MAIN }"><i class="far fa-clock fa-lg" style="margin-right: 5px;"></i>최신순</router-link>
+      </div>
+      <div class="main-link" :class="{ active: listStatus.isLikeList }">
+        <router-link :to="{ name: constants.URL_TYPE.POST.LIKE }"><i class="far fa-heart fa-lg" style="margin-right: 5px;"></i>좋아요</router-link>
+      </div>
+      <div class="main-link" :class="{ active: listStatus.isHitList }">
+        <router-link :to="{ name: constants.URL_TYPE.POST.VIEWS }"><i class="fas fa-fire-alt fa-lg" style="margin-right: 5px;"></i>조회순</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import constants from '@/lib/constants.js'
+
+export default {
+  name: 'PostNavbar',
+  props: {
+    listStatus: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      constants,
+      isRecentList: false,
+      isLikeList: true,
+      isHitList: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+.post-list-link {
+  margin-top: 1rem;
+  display: flex;
+}
+
+.main-link {
+  cursor: pointer;
+  margin-top: 1rem;
+  padding: 10px;
+  border-bottom: 2px solid var(--secondary-color);
+}
+
+.main-link.active {
+  border: none;
+  opacity: 0.2;
+}
+</style>
