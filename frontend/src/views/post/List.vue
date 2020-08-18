4c8dfc8af0dceb7d99d3e74fce1654e80b5ca8ac
kimjanghu <hopewill1030@naver.com>
2020-08-18 20:56:03 +0900

    REFACTORING - edit post lists code

--- frontend/src/views/post/List.vue
+++ frontend/src/views/post/List.vue
@@ -1,109 +1,47 @@
-<template> 
-  <div class="post">
-    <div class="wrapB">
-      <PostNavbar :listStatus="listStatus" />
-      <section class="post-list">
-        <div v-for="(article, index) in articles.list" :key="article.id">
-          <div class="post-card">
-            <a @click="detailPage(article.postId)" >
-              <img
-                :src="article.content"
-                :style="{backgroundImage:'url(https://www.ipcc.ch/site/assets/uploads/sites/3/2019/10/img-placeholder.png)'}"
-                class="post-img"
-              />
-
-              <div class="contents">
-                <h3>{{ article.title }}</h3>
-                <br>
-                <span class="date"><i class="far fa-user"></i> {{ article.nickname }}</span>
-                <br>
-                <span class="date"><i class="far fa-clock"></i> {{ article.createDate }}</span>
-                <br>
-                <span class="date"><i class="far fa-eye"></i> {{ article.hits }}</span>
-                <br>
-                <span class="date"><i class="fas fa-star" style="color: #F2E205;"></i> {{ article.starpoint }}</span>
-                <br>
-                <div class="tag" v-for="(tag, index) in article.hashtag.split(',')" :key="`hash_${index}`">
-                  <span class="tag-btn"># {{ tag }}</span>
-                </div>
-              </div>
-            </a>
-
-            <div class="writer-wrap">
-              <div class="comment-like-wrap">
-                <i class="far fa-comment-alt fa-lg"></i><p style="margin: 0 5px;">{{ articles.comment[index] }}</p>
-              </div>
-              <div v-if="isLoggedIn" class="comment-like-wrap">
-                <i 
-                  :id="`recent_${article.postId}`" 
-                  :ref="article.postId" 
-                  @click.prevent="checkLike(article)" 
-                  v-if="includes(article)" 
-                  class="fas fa-heart fa-lg animated delay-1s redheart" 
-                  style="color: red;"></i>
-                <i 
-                  :id="`recent_${article.postId}`" 
-                  :ref="article.postId" 
-                  @click.prevent="checkLike(article)" 
-                  v-if="!includes(article)" 
-                  class="far fa-heart fa-lg animated infinite bounce delay-1s blankheart" 
-                  style="color: gray;"></i>
-                <p style="margin-left: 5px;">{{ article.likes }}</p>
-              </div>
-              <div v-else class="comment-like-wrap">
-                <i class="fas fa-heart fa-lg redheart" style="color: red;"></i><p style="margin-left: 5px;">{{ article.likes }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+<template>
+  <div>
+    <PostNavbar class="post-navbar" :listStatus="listStatus" />
+    <PostList :likedposts="likedposts" :articles="articles" @check-like="checkLike" />
   </div>
 </template>
 
 <script>
 import '@/assets/css/post.css'
 import axios from 'axios'
-import { mapActions, mapState, mapGetters } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import constants from '@/lib/constants'
 import PostNavbar from '@/components/common/PostNavbar.vue'
+import PostList from '@/components/common/PostList.vue'
+
 
 export default {
   name: 'Post',
   components: {
-    PostNavbar
+    PostNavbar,
+    PostList
   },
   data(){
     return {
       constants,
       SERVER_URL: process.env.VUE_APP_API_URL,
-      likedposts:[],
+      likedposts: [],
       listStatus: {
         isRecentList: false,
         isLikeList: true,
         isHitList: true,
         isStarList: true
       }
-    };
+    }
   },
   computed:{
-    ...mapState(['articles']),
-    ...mapGetters(['isLoggedIn'])
+    ...mapState(['articles'])
   },
   watch: {},
   methods: {
     ...mapActions(['getArticles']),
-    includes(one){
-      if(this.likedposts.includes(one.postId)){
-        return true
-      }
-      else{
-        return false
-      }
-    },
-    checkLike(one){
-      one.userid=this.$cookies.get('auth-token')
-      axios.post(`${this.SERVER_URL}/articles/like`,one)
+    checkLike(post){
+      post.userid = this.$cookies.get('auth-token')
+      axios.post(`${this.SERVER_URL}/articles/like`, post)
       .then(()=>{
         axios.post(`${this.SERVER_URL}/accounts/userDetail`,{"uid":this.$cookies.get('auth-token')})
         .then(res=>{
@@ -111,9 +49,6 @@ export default {
           this.checkLikeList(res)
         })
       })
-    },
-    detailPage(articleId){
-      this.$router.push({ name: constants.URL_TYPE.POST.DETAIL, params: { id: articleId }})
     },
     checkLikeList(res) {
       if (res.data.likedpost) {
@@ -136,7 +71,7 @@ export default {
   },
   updated() {
   },
-};
+}
 </script>
 
 <style scoped>
